81f6b5f704782ccd307752094462212923539114
hardyy7 <milanboy56@yahoo.com>
2024-04-21 21:33:32 +0300

    Lab3

---
+++ Lab3/src/app1/RThread.java
@@ -0,0 +1,25 @@
+package app1;
+
+public class RThread extends Thread{
+    FileService service;
+    public RThread(FileService service) {
+        this.service = service;
+    }
+    public void run(){
+        while (!Main.isStopThreads()){
+            try {
+                String readMsg;
+                synchronized (service) {
+                    readMsg = service.read();
+                }
+                System.out.println(readMsg);
+
+                Thread.sleep(3000);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ Lab3/src/example2/Buffer.java
@@ -0,0 +1,24 @@
+package example2;
+
+import java.util.ArrayList;
+
+public class Buffer {
+    ArrayList<Double> content = new ArrayList<Double>();
+    @SuppressWarnings("removal")
+	synchronized void put(double d){
+        content.add(new Double(d));
+        notify();
+    }
+
+    synchronized double get(){
+        double d =- 1;
+        try{
+            if(content.size() == 0) wait();
+            d = (content.get(0)).doubleValue();
+            content.remove(0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return d;
+    }
+}
