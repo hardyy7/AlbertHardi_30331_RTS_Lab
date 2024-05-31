554d599869e3c94766f5d5ff57a8aee58a895f33
hardyy7 <162709382+hardyy7@users.noreply.github.com>
2024-05-31 21:40:09 +0300

    Lab Session 4

---
+++ LabSession4/src/Example1/Fir.java
@@ -0,0 +1,38 @@
+package Example1;
+
+import java.util.concurrent.Semaphore;
+
+public class Fir extends Thread{
+	
+	int name, delay, k, permit;
+	Semaphore s;
+	Fir(int n, Semaphore sa, int delay, int k, int permit){
+		this.name = n;
+		this.s = sa;
+		this.delay = delay;
+		this.k = k;
+		this.permit = permit;
+	}
+	
+	public void run() {
+		while(true) {
+			try {
+				System.out.println("Fir " + name + " State 1");
+				Thread.sleep(this.delay * 500);
+				System.out.println("Fir " + name + " State 2");
+				this.s.acquire(this.permit);
+				System.out.println("Fir " + name + " took a token from the semaphore");
+				System.out.println("Fir " + name + " State 3");
+				for(int i = 0; i<k*100000;i++) {
+					i++;
+					i--;
+				}
+				this.s.release();
+				System.out.println("Fir " + name + " released a token from the semaphore");
+				System.out.println("Fir " + name + " State 4");
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
